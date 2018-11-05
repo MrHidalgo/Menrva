1cdb34c0eff929415441068cd6117a6b3a56e0ae
MrHidalgo <Luckyhidalgo@gmail.com>
2018-11-05 22:40:16 +0200

    Edits some page.

--- gulp/task/pug.js
+++ gulp/task/pug.js
@@ -3,7 +3,7 @@ const gulp        = require('gulp'),
   pug             = require('gulp-pug'),
   plumber         = require('gulp-plumber'),
   frontMatter     = require('gulp-front-matter'),
-  changedInPlace  = require('gulp-changed-in-place'),
+  emitty          = require('emitty').setup('src/pug', 'pug'),
 	htmlmin         = require('gulp-htmlmin');
 
 
@@ -19,35 +19,38 @@ const configPath  = require('../config/configPath'),
  *
  * @type {*[]}
  */
-const srcPath = [
-  configPath.src.templates + '/*.pug'
-];
+const srcPath = configPath.src.templates + '/*.pug';
 
 
 /**
  * @description Gulp PUG/JADE - preprocessor for creating html files.
  */
-gulp.task('pug', function() {
+const renderPug = () => {
   return gulp
     .src(srcPath)
-      .pipe(plumber(configOption.pipeBreaking.err))
-      .pipe(frontMatter({
-        property: 'data'
-      }))
-      .pipe(pug({
-        pretty: true,
-        data: {
-          env : (argv.prod) ? 'production' : ""
-        },
-      }))
-      .pipe(changedInPlace(configOption.changed))
-      .pipe(gulpif(argv.prod, htmlmin({
-        collapseBooleanAttributes: true,
-        collapseWhitespace: true,
-        removeEmptyAttributes: true,
-        removeComments: true
-      })))
-      .pipe(gulp.dest(configPath.dest.html))
+    .pipe(plumber(configOption.pipeBreaking.err))
+    // .pipe(gulpif(global.isPugWatching, emitty.stream(global.emittyChangedPugFile)))
+    .pipe(emitty.stream(global.emittyChangedPugFile))
+    .pipe(frontMatter({
+      property: 'data'
+    }))
+    .pipe(pug({
+      pretty: true,
+      data: {
+        env : (argv.prod) ? 'production' : ""
+      },
+    }))
+    .pipe(gulpif(argv.prod, htmlmin({
+      collapseBooleanAttributes: true,
+      collapseWhitespace: true,
+      removeEmptyAttributes: true,
+      removeComments: true
+    })))
+    .pipe(gulp.dest(configPath.dest.html));
+};
+
+gulp.task('pug', function() {
+  return renderPug();
 });
 
 
@@ -55,8 +58,9 @@ gulp.task('pug', function() {
  * @description Gulp PUG/JADE watch - keeps track of changes in files.
  */
 gulp.task('pug:watch', function() {
-  gulp.watch(
-    configPath.src.templates + '/**',
-    ['pug']
-  );
+  // global.isPugWatching = true;
+
+  gulp.watch(configPath.src.templates + '/**', ['pug']).on('all', (event, filepath) => {
+    global.emittyChangedPugFile = filepath;
+  });
 });
